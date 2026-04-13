81b25ee20d767f5c0e34c7bdb0754b795dba15c0
Naisarg2024 <24043250@saultcollege.ca>
2026-04-13 17:30:34 -0400

    Initial commit

---
+++ src/main/java/csd230/controllers/SpaController.java
@@ -0,0 +1,17 @@
+package csd230.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SpaController {
+
+    @GetMapping(value = {
+            "/",
+            "/{path:[^\\.]*}",
+            "/**/{path:[^\\.]*}"
+    })
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
